afb840c734ecab8678c5b16698a55aa53a40a874
BorjaG90 <borjagete90@gmail.com>
2019-01-30 09:33:12 +0100

    Add Firsts Admin Configurations

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,8 +1,10 @@
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
+@python_2_unicode_compatible
 class Game(models.Model):
   first_player = models.ForeignKey(User,
     related_name="games_first_player", on_delete=models.CASCADE)
@@ -11,6 +13,11 @@ class Game(models.Model):
   start_time = models.DateTimeField(auto_now_add=True)
   last_active = models.DateTimeField(auto_now=True)
   status = models.CharField(max_length=1, default='F')
+
+  def __str__(self):
+    return "{0} vs {1}".format(
+      self.first_player, self.second_player
+    )
 
 class Move(models.Model):
   x = models.IntegerField()
