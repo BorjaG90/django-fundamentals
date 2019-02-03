420147a4e7e2b63bf6961700f0e90b51dcb62498
BorjaG90 <borjagete90@outlook.es>
2019-02-03 19:30:51 +0100

    Added Custom Model Manager

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,9 +1,22 @@
 from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
+from django.db.models import Q
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class GamesQuerySet(models.QuerySet):
+    def games_for_user(self, user):
+
+      return self.filter(
+        Q(first_player = user) | Q(second_player = user)
+      )
+
+    def active(self):
+      return self.filter(
+        Q(status='F') | Q(status='S')
+      )
 
 GAME_STATUS_CHOICES = (
   ('F', 'First Player To Move'),
@@ -18,11 +31,14 @@ class Game(models.Model):
     related_name="games_first_player", on_delete=models.CASCADE)
   second_player = models.ForeignKey(User,
     related_name="games_second_player", on_delete=models.CASCADE)
+  
   start_time = models.DateTimeField(auto_now_add=True)
   last_active = models.DateTimeField(auto_now=True)
+  
   status = models.CharField(max_length=1, default='F', 
     choices=GAME_STATUS_CHOICES)
 
+  objects = GamesQuerySet.as_manager()
   def __str__(self):
     return "{0} vs {1}".format(
       self.first_player, self.second_player
--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -4,6 +4,9 @@ from gameplay.models import Game
 
 # Create your views here.
 def home(request):
+  my_games = Game.objects.games_for_user(request.user)
+  active_games = my_games.active_games
+
   return render(request, "player/home.html", {
-    'ngames': Game.objects.count()
+    'games': active_games
   })
