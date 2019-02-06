71d39280a5a157eab23a62ae0fa969451be6b0c5
BorjaG90 <borjagete90@gmail.com>
2019-02-06 16:44:19 +0100

    Reestructuration of templates and static files

--- tictactoe/tictactoe/views.py
+++ tictactoe/tictactoe/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def welcome(request):
-  return HttpResponse("Hello, World!")
+  return render(request, 'tictactoe/welcome.html')
