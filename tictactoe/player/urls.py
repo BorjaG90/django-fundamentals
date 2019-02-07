23442507731bf707b54a57931f05b0f5ce496778
BorjaG90 <borjagete90@gmail.com>
2019-02-07 15:39:38 +0100

    Add accept invitations

--- tictactoe/player/urls.py
+++ tictactoe/player/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import home, new_invitation
+from .views import home, new_invitation, accept_invitation
 
 urlpatterns = [
   url(r'home$', home, name="player_home"),
@@ -11,5 +11,7 @@ urlpatterns = [
   url(r'logout$',
     LogoutView.as_view(),
     name="player_logout"),
-  url(r'new_invitation$', new_invitation, name='player_new_invitation')
+  url(r'new_invitation$', new_invitation, name="player_new_invitation"),
+  url(r'accept_invitation/(?P<id>\d+)/$',
+    accept_invitation, name="player_accept_invitation")
 ]
--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 
 from .forms import InvitationForm
 from .models import Invitation
@@ -10,9 +11,10 @@ from gameplay.models import Game
 def home(request):
   my_games = Game.objects.games_for_user(request.user)
   active_games = my_games.active()
-
+  invitations = request.user.invitations_received.all()
   return render(request, "player/home.html", {
-    'games': active_games
+    'games': active_games,
+    'invitations': invitations
   })
 
 @login_required
@@ -26,3 +28,22 @@ def new_invitation(request):
   else:
     form = InvitationForm()
   return render(request, "player/new_invitation_from.html", {'form': form})
+
+@login_required
+def accept_invitation(request, id):
+  invitation = get_object_or_404(Invitation, pk=id)
+  if not request.user == invitation.to_user:
+    raise PermissionDenied
+  if request.method == 'POST':
+    if "accept" in request.POST:
+      game = Game.objects.create(
+        first_player=invitation.to_user,
+        second_player=invitation.from_user,
+      )
+      invitation.delete()
+      return redirect('player_home')
+  else:
+    return render(request,
+      "player/accept_invitation_form.html",
+      {'invitation': invitation}
+    )
